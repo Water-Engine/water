cfe4a9412c9d760a5c23cfe4d0916a230e3f7137
Trevor Swan <tcs94@case.edu>
2025-08-26 19:30:53 -0400

    added loc to README, fixed perft test

--- tests/perft.cpp
+++ tests/perft.cpp
@@ -10,62 +10,53 @@
 TEST_CASE("Single threaded (Position 1)") {
     Bot b;
     b.set_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    REQUIRE(b.perft(1, false) == 20);
-    REQUIRE(b.perft(2, false) == 400);
-    REQUIRE(b.perft(3, false) == 8902);
-    REQUIRE(b.perft(4, false) == 197281);
+    REQUIRE(b.perft(1) == 20);
+    REQUIRE(b.perft(2) == 400);
+    REQUIRE(b.perft(3) == 8902);
+    REQUIRE(b.perft(4) == 197281);
 }
 
 TEST_CASE("Single threaded (Position 2)") {
     Bot b;
     b.set_position("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
-    REQUIRE(b.perft(1, false) == 48);
-    REQUIRE(b.perft(2, false) == 2039);
-    REQUIRE(b.perft(3, false) == 97862);
-    REQUIRE(b.perft(4, false) == 4085603);
+    REQUIRE(b.perft(1) == 48);
+    REQUIRE(b.perft(2) == 2039);
+    REQUIRE(b.perft(3) == 97862);
+    REQUIRE(b.perft(4) == 4085603);
 }
 
 TEST_CASE("Single threaded (Position 3)") {
     Bot b;
     b.set_position("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");
-    REQUIRE(b.perft(1, false) == 14);
-    REQUIRE(b.perft(2, false) == 191);
-    REQUIRE(b.perft(3, false) == 2812);
-    REQUIRE(b.perft(4, false) == 43238);
+    REQUIRE(b.perft(1) == 14);
+    REQUIRE(b.perft(2) == 191);
+    REQUIRE(b.perft(3) == 2812);
+    REQUIRE(b.perft(4) == 43238);
 }
 
 TEST_CASE("Single threaded (Position 4)") {
     Bot b;
     b.set_position("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
-    REQUIRE(b.perft(1, false) == 6);
-    REQUIRE(b.perft(2, false) == 264);
-    REQUIRE(b.perft(3, false) == 9467);
-    REQUIRE(b.perft(4, false) == 422333);
+    REQUIRE(b.perft(1) == 6);
+    REQUIRE(b.perft(2) == 264);
+    REQUIRE(b.perft(3) == 9467);
+    REQUIRE(b.perft(4) == 422333);
 }
 
 TEST_CASE("Single threaded (Position 5)") {
     Bot b;
     b.set_position("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
-    REQUIRE(b.perft(1, false) == 44);
-    REQUIRE(b.perft(2, false) == 1486);
-    REQUIRE(b.perft(3, false) == 62379);
-    REQUIRE(b.perft(4, false) == 2103487);
+    REQUIRE(b.perft(1) == 44);
+    REQUIRE(b.perft(2) == 1486);
+    REQUIRE(b.perft(3) == 62379);
+    REQUIRE(b.perft(4) == 2103487);
 }
 
 TEST_CASE("Single threaded (Position 6)") {
     Bot b;
     b.set_position("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
-    REQUIRE(b.perft(1, false) == 46);
-    REQUIRE(b.perft(2, false) == 2079);
-    REQUIRE(b.perft(3, false) == 89890);
-    REQUIRE(b.perft(4, false) == 3894594);
-}
-
-// ================ MULTI THREADED ================
-
-TEST_CASE("Multi threaded (Position 1)") {
-    Bot b;
-    b.set_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    REQUIRE(b.perft_parallel(4, 8) == 197281);
-    REQUIRE(b.perft_parallel(7, 8) == 3195901860);
+    REQUIRE(b.perft(1) == 46);
+    REQUIRE(b.perft(2) == 2079);
+    REQUIRE(b.perft(3) == 89890);
+    REQUIRE(b.perft(4) == 3894594);
 }
